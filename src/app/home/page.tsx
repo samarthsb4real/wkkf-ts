5b5a97488f8d589f961f5e767726516c1e1b430c
samarthsb4real <samarthbhadane23@gmail.com>
2024-06-13 15:52:40 +0530

    home page updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,101 +1,65 @@
-"use client";
-import { motion } from "framer-motion";
-import React from "react";
-import { ImagesSlider } from "../ui/images-slider";
-import Link from "next/link";
-import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 
-const images = [
-  "https://img.goodfon.com/original/2475x1650/1/f0/kick-karate-fight-training.jpg",
-  "https://images.unsplash.com/photo-1526889588514-2e695856df85?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ];
- 
-const testimonials = [
-    {
-      quote:
-        "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-      name: "Charles Dickens",
-      title: "A Tale of Two Cities",
-    },
-    {
-      quote:
-        "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-      name: "William Shakespeare",
-      title: "Hamlet",
-    },
-    {
-      quote: "All that we see or seem is but a dream within a dream.",
-      name: "Edgar Allan Poe",
-      title: "A Dream Within a Dream",
-    },
-    {
-      quote:
-        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-      name: "Jane Austen",
-      title: "Pride and Prejudice",
-    },
-    {
-      quote:
-        "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-      name: "Herman Melville",
-      title: "Moby-Dick",
-    },
-  ];
 
-export default function HomePage() {
-    return (
-        <div>
-        <ImagesSlider className="h-[40rem]" images={images}>
-          <motion.div
-            initial={{
-              opacity: 0,
-              y: -80,
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-            }}
-            transition={{
-              duration: 0.6,
-            }}
-            className="z-50 flex flex-col justify-left items-center"
-          >
-            <motion.p className="font-bold text-xl md:text-6xl text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-              Welcome to<br /> World Karate & Kobudo Federation
-            </motion.p>
-            <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-              <Link href="/sheets"><span>Join now →</span></Link>
-              <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-            </button>
-          </motion.div>
-        </ImagesSlider>
+import Slider from "../components/Slider";
 
-        <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-        <motion.p className="font-bold text-xl md:text-4xl text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-              Testimonials<br />
-            </motion.p>
-        <InfiniteMovingCards
-          items={testimonials}
-          direction="right"
-          speed="slow"
-        />
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+    <title>Home</title>
+      <div className="m-12 animate-fade-in delay-200">
+        
+        <h1 className="text-5xl font-bold mb-2">
+          Welcome to the World Karate & Kobudo Federation!
+        </h1>
+        <h2 className="text-2xl text-gray-400">
+          Dear Members, Participants, and Supporters,
+        </h2>
+      </div>
+      <div className="flex mr-16 ml-16 mt-12 animate-fade-in delay-400">
+        <div className="flex-col text-lg mr-8 w-3/5">
+          <p className="mb-4">
+            It brings us great pleasure to extend a warm welcome to each and
+            every one of you to the World Karate & Kobudo Federation (WKKF). As
+            an esteemed organization dedicated to the promotion and advancement
+            of karate and kobudo, we are thrilled to have you join our
+            community.
+          </p>
+          <p className="mb-4">
+            At WKKF, we stand at the forefront of martial arts excellence,
+            fostering a spirit of discipline, respect, and camaraderie among our
+            members worldwide. Whether you are a seasoned practitioner, an
+            aspiring athlete, or a passionate enthusiast, you are an invaluable
+            part of our federation.
+          </p>
+          <p className="mb-4">
+            As we embark on this journey together, let us embrace the principles
+            of integrity, perseverance, and mutual support that define our
+            federation. Together, we will strive for excellence in both skill
+            and character, enriching our lives and the lives of those around us
+            through the art of karate and kobudo.
+          </p>
+          <p className="mb-4">
+            We extend our heartfelt gratitude to each of you for your dedication
+            and commitment to the martial arts community. Together, let us
+            uphold the legacy of excellence and sportsmanship that defines the
+            World Karate & Kobudo Federation.
+          </p>
+          <p className="mb-4">
+            Welcome to WKKF, where champions are made, and dreams are realized.
+          </p>
+        </div>
+
+        <div className="flex-col w-2/5 rounded-md">
+          <Slider />
+        </div>
       </div>
 
-
-      
+      <div className="p-16 animate-fade-in delay-800">
+        <img className="rounded-sm border" src="home/11.jpg" />
+        <figcaption className="m-4 text-center text-lg text-gray-300">
+          Event Banner Title of Achievements
+        </figcaption>
       </div>
-      );
+    </div>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,140 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
-import Slider from "./components/Slider";
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "./ui/images-slider";
+import Link from "next/link";
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { HoverEffect } from "./ui/card-hover-effect";
 
-export default function Home() {
+const images = [
+  "https://img.goodfon.com/original/2475x1650/1/f0/kick-karate-fight-training.jpg",
+  "https://images.unsplash.com/photo-1526889588514-2e695856df85?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+export const projects = [
+  {
+    title: "Stripe",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+  },
+  {
+    title: "Netflix",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Google",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+  {
+    title: "Meta",
+    description:
+      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+    link: "https://meta.com",
+  },
+  {
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    title: "Microsoft",
+    description:
+      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+    link: "https://microsoft.com",
+  },
+];
+
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
+
+export default function HomePage() {
   return (
-    <div className="min-h-screen">
-    <title>Home</title>
-      <div className="m-12 animate-fade-in delay-200">
+    <div className="overflow: auto">
+      <ImagesSlider className="h-[45rem]" images={images}>
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: -80,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 0.6,
+          }}
+          className="z-50 flex flex-col justify-left items-center"
+        >
+          <motion.p className="font-bold text-xl md:text-6xl text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+            Welcome to
+            <br /> World Karate & Kobudo Federation
+          </motion.p>
+          <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+            <Link href="/sheets">
+              <span>Join now →</span>
+            </Link>
+            <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+          </button>
+        </motion.div>
+      </ImagesSlider>
+
+      <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <motion.p className="font-bold text-xl md:text-4xl text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+          Testimonials
+          <br />
+        </motion.p>
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+
         
-        <h1 className="text-5xl font-bold mb-2">
-          Welcome to the World Karate & Kobudo Federation!
-        </h1>
-        <h2 className="text-2xl text-gray-400">
-          Dear Members, Participants, and Supporters,
-        </h2>
       </div>
-      <div className="flex mr-16 ml-16 mt-12 animate-fade-in delay-400">
-        <div className="flex-col text-lg mr-8 w-3/5">
-          <p className="mb-4">
-            It brings us great pleasure to extend a warm welcome to each and
-            every one of you to the World Karate & Kobudo Federation (WKKF). As
-            an esteemed organization dedicated to the promotion and advancement
-            of karate and kobudo, we are thrilled to have you join our
-            community.
-          </p>
-          <p className="mb-4">
-            At WKKF, we stand at the forefront of martial arts excellence,
-            fostering a spirit of discipline, respect, and camaraderie among our
-            members worldwide. Whether you are a seasoned practitioner, an
-            aspiring athlete, or a passionate enthusiast, you are an invaluable
-            part of our federation.
-          </p>
-          <p className="mb-4">
-            As we embark on this journey together, let us embrace the principles
-            of integrity, perseverance, and mutual support that define our
-            federation. Together, we will strive for excellence in both skill
-            and character, enriching our lives and the lives of those around us
-            through the art of karate and kobudo.
-          </p>
-          <p className="mb-4">
-            We extend our heartfelt gratitude to each of you for your dedication
-            and commitment to the martial arts community. Together, let us
-            uphold the legacy of excellence and sportsmanship that defines the
-            World Karate & Kobudo Federation.
-          </p>
-          <p className="mb-4">
-            Welcome to WKKF, where champions are made, and dreams are realized.
-          </p>
+      
+      <div className="max-w-5xl  mx-auto px-8">
+      <motion.p className="font-bold text-xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+          About the Organisation
+          <br />
+        </motion.p>
+        
+          <HoverEffect items={projects} />
         </div>
-
-        <div className="flex-col w-2/5 rounded-md">
-          <Slider />
-        </div>
-      </div>
-
-      <div className="p-16 animate-fade-in delay-800">
-        <img className="rounded-sm border" src="home/11.jpg" />
-        <figcaption className="m-4 text-center text-lg text-gray-300">
-          Event Banner Title of Achievements
-        </figcaption>
-      </div>
     </div>
   );
 }
