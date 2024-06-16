d63634e9c009ce29f155bc0cc32e079a205e4115
samarthsb4real <samarthbhadane23@gmail.com>
2024-06-16 18:35:55 +0530

    file config update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+    },
   },
   plugins: [addVariablesForColors, require("daisyui")],
 };
