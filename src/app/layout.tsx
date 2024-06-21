604e452afffddc912651a388aaf032e69501c9d3
samarthsb4real <samarthbhadane23@gmail.com>
2024-06-21 22:51:32 +0530

    navbar updation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Navbar2 from "@/components/Navbar2";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +14,7 @@ export default function RootLayout({
     <html className={inter.className}>
       <body>
         <div>
-          <Navbar2 />
+          <Navbar/>
         </div>
         {children}
         <div>
