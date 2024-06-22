7d4a24830ac7a3f2576ceca8ff9da3b805b830a8
samarthsb4real <samarthbhadane23@gmail.com>
2024-06-23 02:25:15 +0530

    gallery final updation

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,45 +1,65 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { LayoutGrid } from "@/components/ui/layout-grid";
 
 const cards = [
   {
     id: 1,
-    content: "",
-    className: "md:col-span-2 h-full",
-    thumbnail: "/home/1.jpg",
+    content: "Content 1",
+    className: "md:col-span-1 h-48",
+    thumbnail: "/gallery/1.jpg",
   },
   {
     id: 2,
-    content: "",
-    className: "md:col-span-1  h-full",
-    thumbnail: "/home/2.jpg",
+    content: "Content 2",
+    className: "md:col-span-1",
+    thumbnail: "/gallery/2.jpg",
   },
   {
     id: 3,
-    content: "",
-    className: "md:col-span-3 h-full",
-    thumbnail: "/home/3.jpg",
+    content: "Content 3",
+    className: "md:col-span-1",
+    thumbnail: "/gallery/11.jpg",
   },
   {
     id: 4,
-    content: "",
-    className: "md:col-span-1 h-full",
-    thumbnail: "/home/4.jpg",
+    content: "Content 4",
+    className: "md:col-span-1",
+    thumbnail: "/gallery/8.jpg",
   },
-
   {
     id: 5,
-    content: "",
-    className: "md:col-span-2 h-full",
-    thumbnail: "/home/5.jpg",
+    content: "Content 5",
+    className: "md:col-span-1 h-64",
+    thumbnail: "/gallery/9.jpg",
+  },
+  {
+    id: 6,
+    content: "Content 6",
+    className: "md:col-span-1",
+    thumbnail: "/gallery/6.jpg",
+  },
+  {
+    id: 7,
+    content: "Content 7",
+    className: "md:col-span-1",
+    thumbnail: "/gallery/7.jpg",
+  },
+  {
+    id: 8,
+    content: "Content 8",
+    className: "md:col-span-1",
+    thumbnail: "/gallery/8.jpg",
   },
 ];
 
 export default function Gallery() {
   return (
-    <div className="h-screen py-12 w-full">
-      <LayoutGrid cards={cards} />
-    </div>
+    <main className="justify-center items-center h-screen">
+      <div className="container mx-auto py-12">
+        <LayoutGrid cards={cards} />
+        <figcaption className="mt-8 text-gray-500 text-center">Work in Progress...</figcaption>
+      </div>
+    </main>
   );
 }
