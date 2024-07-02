073420ef902cbcc4ebfa146c4a24cf5273fc2f2c
samarthsb4real <samarthbhadane23@gmail.com>
2024-07-02 22:02:44 +0530

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -141,7 +141,7 @@ export default function HomePage() {
               </motion.p>
               <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
                 <Link href="/about">
-                  <span>Learn More about us</span>
+                  <span>Learn More about Us</span>
                 </Link>
                 <div className="absolute inset-x-0 h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
               </button>
